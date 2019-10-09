d6234760bce4e9f26a5c9191e7a8fa405f46c3a7
Camila Ines Sosa <camila.sosa@neoris.cxnetworks.net>
2019-10-09 09:22:12 -0300

    Fin de sesión implementado

--- src/efixRouter.js
+++ src/efixRouter.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
-const router = express.Router();
 const bodyParser = require('body-parser');
 const userServices = require('./DBservices/UserService');
 const orderService = require('./DBservices/OrderService');
+const router = express.Router();
 
 //------------------MIDDLEWARE SETUP------------------//
 
 const host = 'http://localhost:3000' //When accessing locally
 //const host = 'http://10.15.77.95:3000' // When accessing from another network (public IP) or from another pc (local IP)
 
-router.use(function(req, res, next) {
+router.use((_req, res, next) => {
     res.header("Access-Control-Allow-Origin", host);
-    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
   });
@@ -33,7 +32,7 @@ router.use(session({
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.use(function (req, res, next) {
+router.use((_req, _res, next) => {
     next();
  });
 
@@ -43,7 +42,7 @@ router.post('/auth', (req, res) => {
     user = req.body.user;
     pass = req.body.pass;
     userServices.authUser(user, pass, (result) => {
-        if(result !== undefined){  
+        if(result !== undefined){ 
 			req.session.loggedin = true;
 			req.session.username = user;
             res.status(200).send({});
@@ -55,6 +54,13 @@ router.post('/auth', (req, res) => {
 
 router.get('/isAuthored', (req, res) => {
     req.session.loggedin ? res.status(200).send({}) : res.status(401).send({})
+})
+
+router.get('/endSession', (req, res) => {
+    req.session.destroy((err) =>{
+        if(err) console.log(err)
+        res.status(200).send({})
+    })
 })
 
 //------------------ORDER-RELATED REQUESTS------------------//
@@ -78,6 +84,7 @@ router.post('/saveOrder', (req, res) => {
 
 router.get('/getAllOrders', (req, res) => {
     if(req.session.loggedin) {
+        console.log(req.session.cookie)
         orderService.getAllOrders((result) =>{
             res.status(200).send({result})
         })
@@ -106,5 +113,6 @@ router.get('/search/:string', (req, res) =>{
     }
 })
      
+//------------------AUXILIAR METHODS------------------//
 
 module.exports = router;
