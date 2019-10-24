345a087a92f13e40d1f17989ec083d00c801cb2a
CamilaSosa96 <camila_ines_sosa@hotmail.com>
2019-10-24 10:19:07 -0300

    Refactor

--- src/efixRouter.js
+++ src/efixRouter.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 //------------------MIDDLEWARE SETUP------------------//
 
-let host = ""
+let host = ''
 
 router.use((req, res, next) => {
     host = req.headers.origin
@@ -31,7 +31,7 @@ router.use(session({
 }));
 
 router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.urlencoded({extended: true}))
 
 router.use((_req, _res, next) => {
     next()
@@ -40,16 +40,15 @@ router.use((_req, _res, next) => {
 //------------------USER-RELATED REQUESTS------------------//
 
 router.post('/auth', (req, res) => {
-    user = req.body.user;
-    pass = req.body.pass;
+    user = req.body.user
+    pass = req.body.pass
     userService.authUser(user, pass, (result) => {
         if(result !== undefined){ 
 			req.session.loggedin = true
 			req.session.username = user
-            res.status(200).send({})
-        } else {
-            res.status(401).send({})
-        }
+            res.status(200).send()
+        } 
+        else res.status(401).send()
     })
 })
 
@@ -60,18 +59,17 @@ router.get('/isAuthored', (req, res) => {
 router.get('/endSession', (req, res) => {
     req.session.destroy((err) => {
         if(err) console.log(err)
-        res.status(200).send({})
+        res.status(200).send()
     })
 })
 
 router.post('/newUser', (req, res) => {
     if(req.session.loggedin && (req.session.username === 'Admin')){
-        userService.createUser(req.body.user, req.body.pass, (error) => {
-            if(error) res.status(409).send({})
-            else res.status(201).send({})
+        userService.createUser(req.body.user, req.body.pass, (err) => {
+            err ? res.status(409).send() : res.status(201).send()
         })   
     }
-    else res.status(403).send({})
+    else res.status(403).send()
 })
 
 //------------------ORDER-RELATED REQUESTS------------------//
@@ -87,7 +85,7 @@ router.post('/saveOrder', (req, res) => {
         model = req.body.productModel
         problem = req.body.problem
         orderService.saveOrder(user, name, dni, email, type, brand, model, problem, (_result) => {
-            res.status(201).send({})
+            res.status(201).send()
         })
     })
 })
@@ -95,7 +93,7 @@ router.post('/saveOrder', (req, res) => {
 router.get('/getAllOrders', (req, res) => {
     doIfAuthored(req, res, () => {
         orderService.getAllOrders((result) => {
-            res.status(200).send({result})
+            res.status(200).send(result)
         })
     })
 })
@@ -104,7 +102,7 @@ router.post('/updateState/:id/:state', (req, res) => {
     doIfAuthored(req, res, () => {    
         doIfOwner(req.params.id, req.session.username, res, () => {
             orderService.updateState(req.params.id, req.params.state, (_result) => {
-                res.status(200).send({})
+                res.status(200).send()
             })
         })
     })        
@@ -114,54 +112,46 @@ router.post('/loadBudget', (req, res) => {
     doIfAuthored(req, res, () => {
         doIfOwner(req.body.id, req.session.username, res, () => {
             orderService.loadBudget(req.body.id, req.body.diagnosis, req.body.budget, (_result) => {
-                res.status(200).send({})
+                res.status(200).send()
             })
         })
     })
 })
     
-router.get('/search/:string', (req, res) =>{
+router.get('/search/:string', (req, res) => {
     doIfAuthored(req, res, () => {
         orderService.searchOrderByEmail(req.params.string, (result) => {
-            res.status(200).send({result})
+            res.status(200).send(result)
         })
     })
 })
 
 router.get('/budgetApproval/:dni/:id', (req, res) => {
-    orderService.getOrderById(req.params.id, req.params.dni, (result) =>{
-        if(result[0] === undefined){
-            res.status(404).send({})
-        } else {
-            res.status(200).send(result[0])
-        }
+    orderService.getOrderById(req.params.id, req.params.dni, (result) => {
+        result[0] === undefined ? res.status(404).send() : res.status(200).send(result[0])
     })
 })
 
 router.post('/clientResponse/:id/:dni', (req, res) => {
     const state = req.body.choice ? 'REPARACION' : 'RETIRAR_SINARREGLO'
     orderService.updateState(req.params.id, state, (_result) => {
-        res.status(200).send({})
+        res.status(200).send()
     })
 })
 
 router.get('*', (_req, res) => {
-    res.status(404).send({})
+    res.status(404).send()
 })
 
 //------------------AUXILIAR METHODS------------------//
 
 function doIfAuthored(req, res, callback){
-    if(req.session.loggedin){
-         callback()
-    }
-    else res.status(401).send({})
+    req.session.loggedin ? callback() : res.status(401).send({})
 }
 
 function doIfOwner(id, sessionName, res, callback){
     orderService.isOwner(id, sessionName, (result) => {
-          if(result || sessionName === 'Admin') callback()
-          else res.status(403).send({}) 
+          result || sessionName === 'Admin' ? callback() : res.status(403).send({}) 
     })
 }
 
